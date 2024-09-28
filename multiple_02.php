8fdcd14ec15e3395c4929fa6de70df158004d8a4
jcrab <jcrab@acm.org>
2024-09-28 10:47:56 -0500

    finished

--- multiple_02.php
+++ multiple_02.php
@@ -1,20 +1,36 @@
+<?php session_start();
+//echo "this is NOT ignored";
+//var_dump($_POST);
+if (!isset($_POST['first_name'])) {
+    header("Location: index.html");
+}
+
+$fn = $_POST['first_name'];
+$ln = $_POST['family_name'];
+
+$_SESSION['first'] = $fn;
+$_SESSION['last'] = $ln;
+
+$someone = $fn . " " . $ln;
+?>
 <!DOCTYPE HTML>
 <html>
-<head>
-<title>Multiple form 2</title>
-</head>
+    <head>
+        <title>Multiple form 2</title>
+    </head>
 
-<body>
-<!-- say hello to the user here -->
+    <body>
+        <!-- say hello to the user here -->
+        <?php echo "Hello, $someone" ?>
 
-<form action="" method="get">
-<p>
-<label for="age">Age:</label>
-<input type="number" name="age" id="age_id">
-</p>
-<p>
-<input type="submit" name="next" value="Next &gt;">
-</p>
-</form>
-</body>
+        <form action="multiple_03.php" method="GET">
+            <p>
+                <label for="age">Age:</label>
+                <input type="number" name="age" id="age_id">
+            </p>
+            <p>
+                <input type="submit" name="next" value="Next &gt;">
+            </p>
+        </form>
+    </body>
 </html>
--- multiple_04.php
+++ multiple_04.php
@@ -1,1 +1,32 @@
-<!DOCTYPE HTML> <html> <head> <title>Results</title> </head> <body> <p>The details submitted were as follows: </p> <ul> </ul> </body> </html>
+<?php session_start();
+if (!isset($_SESSION['first'])) {
+    header("Location: index.html");
+}
+$first = $_SESSION['first'];
+$last = $_SESSION['last'];
+$age = $_SESSION['age'];
+
+$addr = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$country = $_POST['country'];
+?>
+<!DOCTYPE HTML> 
+<html> 
+    <head> 
+        <title>Results</title> 
+    </head> 
+    <body> 
+        <p>The details submitted were as follows: 
+        </p> 
+        <ul> 
+        <li><?= $first ?></li>
+        <li><?= $last ?></li>
+        <li><?= $age ?></li>
+        <li><?= $addr ?></li>
+        <li><?= $city ?></li>
+        <li><?= $state ?></li>
+        <li><?= $country ?></li>
+        </ul> 
+    </body> 
+</html>
